e318b82310791a281ef34c3f605591293a32673b
rcatalan98 <robertitocatalan@gmail.com>
2021-06-06 19:31:40 -0300

    Trigger added.

--- funciones.sql
+++ funciones.sql
@@ -26,3 +26,38 @@ CREATE TABLE definitiva(
 
 --Funciones trigger
 
+CREATE OR REPLACE FUNCTION fillTable() RETURNS TRIGGER AS
+
+    $$
+        DECLARE
+            auxYear text;
+            auxMonth text;
+            auxDay text;
+            auxDate date;
+            auxStr text;
+        BEGIN
+            auxYear := split_part(new.Quarter, '/', 2);
+            auxMonth := split_part(new.Month, '-', 2);
+            auxMonth := to_char(to_date(auxMonth, 'Mon'), 'MM' );
+            auxDay :=  split_part(new.Week, '-', 1);
+
+            case  when auxDay = 'W1' then auxDay := '01';
+                when auxDay = 'W2' then auxDay := '08';
+                when auxDay = 'W3' then auxDay := '15';
+                when auxDay = 'W4' then auxDay := '22';
+                when auxDay = 'W5' then auxDay := '29';
+            end case;
+
+            auxStr := concat(auxYear,auxMonth,auxDay);
+            auxDate := to_date(auxStr, 'YYYYMMDD');
+
+            INSERT INTO definitiva(Sales_Date,Product_type, Territory,Sales_Channel,Customer_type, Revenue, Cost) VALUES (auxDate, new.Product_type, new.Territory, new.Sales_Channel, new.Customer_type, new.Revenue, new.Cost);
+            RETURN new;
+        END
+
+    $$ LANGUAGE plpgsql;
+
+CREATE TRIGGER fillTableTrigger
+    AFTER INSERT ON intermedia
+    FOR EACH ROW
+    EXECUTE PROCEDURE fillTable();
